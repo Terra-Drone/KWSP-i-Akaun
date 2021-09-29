1706bd523c78c1ef74bcb1ae06d410f12a9cfb57
Anas Thahiri <61678345+Terra-Drone@users.noreply.github.com>
2021-09-29 11:27:26 +0800

    Update function.js

--- secured/js/function.js
+++ secured/js/function.js
@@ -3,26 +3,29 @@ window.onscroll = function() {stickyNav()};
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 function stickyNav() {
-  navbar.style.transition= "150ms ease";
+  navbar.style.transition= "350ms ease";
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
 //Hide options on small screen
-var navi = document.getElementById("navi");
 function hideDrop() {
+  var navi = document.getElementById("navi")
+  navbar.style.transition= "350ms ease";
   if (navi.className === "rc_nav") {
-      navi.classList.add("responsive");
+      navbar.classList.add("responsive");
   } else {
       navi.classList.remove("responsive");
   }
 }
-//Dim screen
-//var dropdown = document.getElementsByClassName('dropdown');
-//var dim = document.getElementsByClassName('dim');
 
+//Dim screen
+var dim = document.getElementsByClassName("dim");
+navi.onmouseover = function() { dim.style.display = "block"; }
+navi.onmouseout = function() { dim.style.display = "none";  }
 //FORGOT LOGIN
 function recoverLogin() {
 
