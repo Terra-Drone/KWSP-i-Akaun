0c9b2bea6776d6cea14bf0dfa2ef157051005987
Anas <anasthahiri1997@gmail.com>
2021-11-12 08:52:14 +0800

    Update Nov 12, 2021

--- secured/js/function.js
+++ secured/js/function.js
@@ -32,10 +32,58 @@ function hideDrop() {
   }
 }
 
+//LOGIN
+
+
 //MAIN PAGE
 //Date for chart
 window.onload = function showDate() {
   today = new Date();
   var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-  document.getElementById("date").innerHTML = date;
 }
+
+var data = {
+  labels: [
+    "Akaun 1",
+    "Akaun 2"
+  ],
+  datasets: [
+    {
+      data: [12,2],
+      backgroundColor: [
+        "#430092",
+        "#36A2EB"
+      ],
+      hoverBackgroundColor: [
+        "#430092",
+        "#36A2EB"
+      ]
+    }]
+};
+
+var akaunBalance = new Chart(document.getElementById('myAkaun'), {
+  type: 'doughnut',
+  data: data,
+  options: {
+  	responsive: true,
+    legend: { display: false }
+  }
+});
+
+Chart.pluginService.register({
+  beforeDraw: function(chart) {
+    var width = chart.chart.width,
+        height = chart.chart.height,
+        ctx = chart.chart.ctx;
+
+    ctx.restore();
+    var fontSize = (height / 114).toFixed(2);
+    ctx.font = fontSize + "em sans-serif";
+    ctx.textBaseline = "middle";
+
+    var text = showDate();
+
+    ctx.fillText(text);
+    ctx.save();
+  }
+});
